5a8448c34144d98c2748ac1bf297e6c4d78cee7c
Danie Banks <daniebanks718@gmail.com>
2015-12-22 16:33:09 -0600

    created goals controller, model and views using scaffolding

---
+++ app/views/goals/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @goal, :id, :title, :description, :complete, :created_at, :updated_at
---
+++ spec/factories/goals.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :goal do
+    title "MyString"
+description "MyText"
+complete false
+  end
+
+end
