eb5a25dbdaa5a62f1995ed26266d725d76bc5dba
Danie Banks <daniebanks718@gmail.com>
2015-12-23 12:24:43 -0600

    added bootstrap and updated home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   resources :goals
-  root 'welcome#index'
+  root 'goals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
