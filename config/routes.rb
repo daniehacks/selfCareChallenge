eddf7ae662196885b6cf981b897d6d2a49c835e0
Danie Banks <daniebanks718@gmail.com>
2016-01-02 19:56:25 -0600

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   resources :goals
 
-  root 'goals#index'
+  root 'pages#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
